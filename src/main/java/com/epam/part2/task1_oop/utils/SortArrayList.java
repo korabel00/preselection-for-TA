63c00b0677be502b9ba34914febc02f4fb793786
ilia_solovev <ilia_solovev@epam.com>
2021-02-25 19:33:47 +0400

    Перевел на 8 Java

--- src/main/java/com/epam/part2/task1_oop/utils/SortArrayList.java
+++ src/main/java/com/epam/part2/task1_oop/utils/SortArrayList.java
@@ -2,23 +2,22 @@ package com.epam.part2.task1_oop.utils;
 
 import com.epam.part2.task1_oop.home_appliances.HomeAppliances;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class SortArrayList {
 
-    public static Comparator<HomeAppliances> POWER = new Comparator<>() {
-
-        @Override
-        public int compare(HomeAppliances ha1, HomeAppliances ha2) {
-            return ha1.getPowerConsumption() - ha2.getPowerConsumption();
-        }
-    };
+//    public static Comparator<HomeAppliances> POWER = new Comparator<>() {
+//
+//        @Override
+//        public int compare(HomeAppliances ha1, HomeAppliances ha2) {
+//            return ha1.getPowerConsumption() - ha2.getPowerConsumption();
+//        }
+//    };
 
     public static void sortByPowerConsumption(List<HomeAppliances> arrayList) {
 
-        arrayList.sort(SortArrayList.POWER);
-        output(arrayList);
+//        arrayList.sort(SortArrayList.POWER);
+//        output(arrayList);
     }
 
     private static void output(List<HomeAppliances> arrayList) {
